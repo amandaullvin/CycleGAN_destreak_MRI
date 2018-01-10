bdc04247c354bb848542ad60106d39ad3314e92f
emredog <emredog@gmail.com>
2018-01-10 21:27:24 +0300

    continue train

--- scripts/train_cyclewgan.sh
+++ scripts/train_cyclewgan.sh
@@ -1,1 +1,1 @@
-python3.5 train_wgan.py --dataroot ./datasets/RainRemoval --name rainremoval_cyclewgan02 --model cycle_wgan --no_dropout --batchSize 3 --display_id 0 --nepoch 100 --nepoch_decay 100 --lambda_A 10.0 --lambda_B 10.0 --lambda_feat 2.0 --print_freq 1
+python3.5 train_wgan.py --dataroot ./datasets/RainRemoval --name rainremoval_cyclewgan02 --model cycle_wgan --no_dropout --batchSize 3 --display_id 0 --nepoch 100 --nepoch_decay 100 --lambda_A 10.0 --lambda_B 10.0 --lambda_feat 2.0 --print_freq 1 --continue_train
