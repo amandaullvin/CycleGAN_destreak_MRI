d3fe0f0128d26cbbe89faf7b4d4e113723586e6f
emredog <emredog@gmail.com>
2018-01-11 18:58:51 +0300

    trying DCGAN as discriminator

--- models/networks.py
+++ models/networks.py
@@ -72,6 +72,8 @@ def define_D(input_nc, ndf, which_model_netD,
         netD = NLayerDiscriminator(input_nc, ndf, n_layers=3, norm_layer=norm_layer, use_sigmoid=use_sigmoid, gpu_ids=gpu_ids)
     elif which_model_netD == 'n_layers':
         netD = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer, use_sigmoid=use_sigmoid, gpu_ids=gpu_ids)
+    elif which_model_netD == 'dcgan':
+        netD = DCGAN_D(input_nc, ndf, 256, gpu_ids) # FIXME! isize instead of 256!!
     else:
         raise NotImplementedError('Discriminator model name [%s] is not recognized' %
                                   which_model_netD)
@@ -329,6 +331,56 @@ class UnetSkipConnectionBlock(nn.Module):
         else:
             return torch.cat([self.model(x), x], 1)
 
+
+class DCGAN_D(nn.Module):
+    def __init__(self, input_nc, ndf, isize, gpu_ids=[], n_extra_layers=0):
+        super(DCGAN_D, self).__init__()
+        self.gpu_ids = gpu_ids
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        main = nn.Sequential()
+        # input is nc x isize x isize
+        main.add_module('initial.conv.{0}-{1}'.format(input_nc, ndf),
+                        nn.Conv2d(input_nc, ndf, 4, 2, 1, bias=False))
+        main.add_module('initial.relu.{0}'.format(ndf),
+                        nn.LeakyReLU(0.2, inplace=True))
+        csize, cndf = isize / 2, ndf
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}.{1}.conv'.format(t, cndf),
+                            nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}.{1}.batchnorm'.format(t, cndf),
+                            nn.BatchNorm2d(cndf))
+            main.add_module('extra-layers-{0}.{1}.relu'.format(t, cndf),
+                            nn.LeakyReLU(0.2, inplace=True))
+
+        while csize > 4:
+            in_feat = cndf
+            out_feat = cndf * 2
+            main.add_module('pyramid.{0}-{1}.conv'.format(in_feat, out_feat),
+                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
+            main.add_module('pyramid.{0}.batchnorm'.format(out_feat),
+                            nn.BatchNorm2d(out_feat))
+            main.add_module('pyramid.{0}.relu'.format(out_feat),
+                            nn.LeakyReLU(0.2, inplace=True))
+            cndf = cndf * 2
+            csize = csize / 2
+
+        # state size. K x 4 x 4
+        main.add_module('final.{0}-{1}.conv'.format(cndf, 1),
+                        nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
+        self.main = main
+
+
+    def forward(self, input):
+        if len(self.gpu_ids) and isinstance(input.data, torch.cuda.FloatTensor):
+            output = nn.parallel.data_parallel(self.main, input, self.gpu_ids)
+        else: 
+            output = self.main(input)
+            
+        output = output.mean(0)
+        return output.view(1)
 
 # Defines the PatchGAN discriminator with the specified arguments.
 class NLayerDiscriminator(nn.Module):
