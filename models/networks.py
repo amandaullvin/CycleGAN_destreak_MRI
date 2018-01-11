95adac12f7e6e090d752538e706850e144e8930b
emredog <emredog@gmail.com>
2018-01-11 20:41:30 +0300

    reformatting the naming for DCGAN

--- models/networks.py
+++ models/networks.py
@@ -340,35 +340,35 @@ class DCGAN_D(nn.Module):
 
         main = nn.Sequential()
         # input is nc x isize x isize
-        main.add_module('initial.conv.{0}-{1}'.format(input_nc, ndf),
+        main.add_module('initial_conv_{0}-{1}'.format(input_nc, ndf),
                         nn.Conv2d(input_nc, ndf, 4, 2, 1, bias=False))
-        main.add_module('initial.relu.{0}'.format(ndf),
+        main.add_module('initial_relu_{0}'.format(ndf),
                         nn.LeakyReLU(0.2, inplace=True))
         csize, cndf = isize / 2, ndf
 
         # Extra layers
         for t in range(n_extra_layers):
-            main.add_module('extra-layers-{0}.{1}.conv'.format(t, cndf),
+            main.add_module('extra-layers-{0}_{1}.conv'.format(t, cndf),
                             nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
-            main.add_module('extra-layers-{0}.{1}.batchnorm'.format(t, cndf),
+            main.add_module('extra-layers-{0}_{1}.batchnorm'.format(t, cndf),
                             nn.BatchNorm2d(cndf))
-            main.add_module('extra-layers-{0}.{1}.relu'.format(t, cndf),
+            main.add_module('extra-layers-{0}_{1}.relu'.format(t, cndf),
                             nn.LeakyReLU(0.2, inplace=True))
 
         while csize > 4:
             in_feat = cndf
             out_feat = cndf * 2
-            main.add_module('pyramid.{0}-{1}.conv'.format(in_feat, out_feat),
+            main.add_module('pyramid_{0}-{1}_conv'.format(in_feat, out_feat),
                             nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
-            main.add_module('pyramid.{0}.batchnorm'.format(out_feat),
+            main.add_module('pyramid_{0}_batchnorm'.format(out_feat),
                             nn.BatchNorm2d(out_feat))
-            main.add_module('pyramid.{0}.relu'.format(out_feat),
+            main.add_module('pyramid_{0}_relu'.format(out_feat),
                             nn.LeakyReLU(0.2, inplace=True))
             cndf = cndf * 2
             csize = csize / 2
 
         # state size. K x 4 x 4
-        main.add_module('final.{0}-{1}.conv'.format(cndf, 1),
+        main.add_module('final_{0}-{1}_conv'.format(cndf, 1),
                         nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
         self.main = main
 
