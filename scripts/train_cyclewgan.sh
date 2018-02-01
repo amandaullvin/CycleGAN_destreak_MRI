135719fa5f7132230f590b2ab8c1025dc3271e04
emredog <emredog@gmail.com>
2018-02-01 17:00:37 +0300

    dcgan and default resnet

--- scripts/train_cyclewgan.sh
+++ scripts/train_cyclewgan.sh
@@ -1,4 +1,4 @@
-python train_wgan.py --dataroot ./datasets/RainRemoval --name RainRemoval17  --model cycle_wgan --no_dropout --batchSize 1 --pool_size 5 --adam --norm batch --which_model_netD dcgan --which_model_netG unet_128 --lr 0.00005 --nepoch 100 --nepoch_decay 150 --lambda_A 5.0 --lambda_B 5.0 --lambda_feat 0.0 --identity 0.0 --print_freq 1 --display_id -1
+python train_wgan.py --dataroot ./datasets/RainRemoval --name RainRemoval50  --model cycle_wgan --no_dropout --batchSize 1 --pool_size 5 --no_html --adam --norm batch --which_model_netD dcgan --lr 0.00005 --nepoch 100 --nepoch_decay 150 --lambda_A 5.0 --lambda_B 5.0 --lambda_feat 0.0 --identity 0.0 --print_freq 1 --display_id -1
 #--norm batch
 #--which_model_netD dcgan
 #--which_model_netG resnet_6blocks
